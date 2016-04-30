5a5d1880ca7f14b9e3b520c3d32bf3bbe564af15
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-04-30 19:59:06 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,49 @@
+
+def using_push (array,item)
+  array.push(item)
+end
+
+def using_unshift(array,item)
+  array.unshift(item)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def  pop_with_args (array)
+  array.pop(2)
+end
+
+def using_shift(a)
+  a.shift
+end
+
+def shift_with_args(a)
+  a.shift(2)
+end
+
+def using_concat(a,b)
+  a.concat(b)
+end
+
+def using_insert(a,b)
+  a.insert(4,b)
+end
+
+def using_uniq(a)
+  a.uniq
+end
+
+def using_flatten(a)
+  a.flatten
+end
+
+def using_delete(a,item)
+  a.delete(item)
+end
+
+def using_delete_at(a,index)
+  a.delete_at(2)
+end
+
